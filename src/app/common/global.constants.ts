57f79e042d1af1830528462d71f6e0273c865369
ajmalkambrath <ajmalkambrath@gmail.com>
2023-04-03 17:18:29 +0400

    updated review comments

--- src/app/common/global.constants.ts
+++ src/app/common/global.constants.ts
@@ -1,4 +1,3 @@
 export const DEFAULT_INTERVAL = 1000;
 export const DEFAULT_SIZE = 100;
 export const DEFAULT_DATA_UI_SIZE = 10;
-export const DASHBOARD_WORKER_URL = '../../../common/worker/dashboard.worker';
